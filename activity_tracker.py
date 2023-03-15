f60dcbaec0c0d248e78dec09cdfa768ef4e4feb5
Kevin Cantwell <kevin.cantwell@gmail.com>
2023-03-15 12:24:59 -0400

    asked chatgpt to debug and make some edits

--- activity_tracker.py
+++ activity_tracker.py
@@ -1,9 +1,34 @@
 import tkinter as tk
-from tkinter import messagebox
+from tkinter import simpledialog
 import sqlite3
 from datetime import datetime
 import time
-import threading
+import multiprocessing
+
+class ActivityDialog(simpledialog.Dialog):
+    def __init__(self, parent, title=None):
+        self.result = None
+        simpledialog.Dialog.__init__(self, parent, title)
+
+    def body(self, master):
+        activities = ["Gmail", "Code", "Slack", "Reading Docs", "Writing Docs", "Making Slides", "Procrastinating"]
+        row = 0
+        col = 0
+
+        for activity in activities:
+            button = tk.Button(master, text=activity, width=20, command=lambda a=activity: self.set_result(a))
+            button.grid(row=row, column=col)
+
+            col += 1
+            if col > 2:
+                col = 0
+                row += 1
+
+        return None
+
+    def set_result(self, activity):
+        self.result = activity
+        self.ok()
 
 def setup_database():
     conn = sqlite3.connect("activities.db")
@@ -24,15 +49,11 @@ def record_activity(activity):
 def prompt_user():
     root = tk.Tk()
     root.withdraw()
-
-    activities = ["Gmail", "Code", "Slack", "Reading Docs", "Writing Docs", "Making Slides", "Procrastinating"]
-    activity = messagebox.askquestion("Activity Tracker", "What are you doing?", type="custom", default="Gmail",
-                                      custom_buttons=activities)
-
-    if activity != "cancel":
-        record_activity(activity)
-
+    activity = ActivityDialog(root, "Activity Tracker").result
     root.destroy()
+
+    if activity:
+        record_activity(activity)
 
 def scheduler():
     current_hour = datetime.now().hour
@@ -43,5 +64,6 @@ def scheduler():
 
 if __name__ == "__main__":
     setup_database()
-    activity_tracking_thread = threading.Thread(target=scheduler)
-    activity_tracking_thread.start()
+    scheduler_process = multiprocessing.Process(target=scheduler)
+    scheduler_process.start()
+    scheduler_process.join()
